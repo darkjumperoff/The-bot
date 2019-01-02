23c04c6bc4541fda3a73e2bb573fcb683a301bad
darkjumperoff <46317637+darkjumperoff@users.noreply.github.com>
2019-01-02 16:04:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ bot.on("message", async function(message) {
     var args = message.content.substring(PREFIX.length).split(" ");
 
     switch(args[0].toLowerCase()) {
-        case "channel":
+        case "creator":
         message.channel.send("", {
             embed: {
                 color: 0xF0000,
@@ -65,6 +65,32 @@ bot.on("message", async function(message) {
         break;
     }
   
+});
+bot.on('message',message =>{
+    if(message.content === prefix + "info"){
+        message.reply("[Date]:02/01/19 [hours]:15:30");
+        console.log("commande effectuée");
+    }
+   
+
+    if(message.content === prefix + "help"){
+        message.channel.sendMessage("list of commands: D!info|D!invite|D!creator");
+        console.log("commande effectuée");
+        
+    }
+
+    if(message.content === prefix + "Friends"){
+        message.reply(":heart:*Flemin Bilou Gaytora Tedy EModzz Misterpanda Tweesch Erwan Valentin Smz_timber Piratennaol*:heart");
+        console.log("commande effectuée");
+    }
+
+    if(message.content === prefix + "idea"){
+        message.reply("if you have somes idea tell to my creator")
+        console.log("commande effectuée")
+    }
+
+
+
 });
 
 bot.login(process.env.TOKEN);
