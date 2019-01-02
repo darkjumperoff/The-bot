b00545b548aff71ef816b9c1954d9bd52b0c73ed
darkjumperoff <46317637+darkjumperoff@users.noreply.github.com>
2019-01-02 13:32:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const Discord = require("discord.js");
 
+var PREEFIX = "D!";
+
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
     bot.user.setGame("The bot, D!help");
     console.log("Le bot est connecté");
+});
+
+bot.once("message", async function (message){
+    if (message.author.equals(bot.user)) return; 
+
+    if(!message.content.startsWith(PREFIX)) return; 
+
+    var args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0].toLowerCase()) {
+
+    }
+case "invite":
+message.channel.send("test")
+break;
+
+//BREAK
+//DEFAULT
+
+
 });
 
 bot.login("process.env.TOKEN");
