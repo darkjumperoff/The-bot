a952f1ac7de3d8c96388fe47358b5a24a5f01cc8
darkjumperoff <46317637+darkjumperoff@users.noreply.github.com>
2019-01-02 14:01:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -43,6 +43,47 @@ break;
 //DEFAULT
 
 
+
+
+
+const Discord = require("discord.js");
+
+var PREFIX = "!";
+
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+    bot.user.setGame("TutoBot, !help");
+    console.log("Le bot a bien ete connecte");
 });
 
-bot.login("process.env.TOKEN");
+bot.on("message", async function(message) {
+    if (message.author.equals(bot.user)) return;
+
+    if(!message.content.startsWith(PREFIX)) return;
+
+    var args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0].toLowerCase()) {
+        case "invite":
+        message.channel.send("", {
+            embed: {
+                color: 0xFF0000,
+                author: message.author.name,
+                title: '',
+                fields: [{
+                    name: "Lien d'invitation discord",
+                    value: "https://discord.gg/CsH3kj",
+                    inline: false
+                }],
+                footer: {
+                    footer: "Partager ce lien à tous vos amis !",
+                },
+            }
+        });
+        break;
+    }
+});
+
+bot.login(process.env.TOKEN);
+ 
