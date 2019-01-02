24d4b797213b59ca328c6db150a00bd0a69f289b
darkjumperoff <46317637+darkjumperoff@users.noreply.github.com>
2019-01-02 14:17:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ bot.on("message", async function(message) {
         case "invite":
         message.channel.send("", {
             embed: {
-                color: 0xFF0000,
+                color: 0xF0000,
                 author: message.author.name,
                 title: '',
                 fields: [{
@@ -28,14 +28,42 @@ bot.on("message", async function(message) {
                     value: "https://discord.gg/gFXxhyw",
                     inline: false
                 }],
-                footer: {
+                footer: { 
                     footer: "share thislink with your friends !",
                 },
             }
         });
         break;
     }
+  
+});
+bot.on("message", async function(message) {
+    if (message.author.equals(bot.user)) return;
+
+    if(!message.content.startsWith(PREFIX)) return;
+
+    var args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0].toLowerCase()) {
+        case "channel":
+        message.channel.send("", {
+            embed: {
+                color: 0xF0000,
+                author: message.author.name,
+                title: '',
+                fields: [{
+                    name: "the channel of my creator",
+                    value: "https://www.youtube.com/channel/UCDt-Z_j3Wzm5YU1Z47_hNjA?view_as=subscriber",
+                    inline: false
+                }],
+                footer: { 
+                    footer: "share thislink with your friends !",
+                },
+            }
+        });
+        break;
+    }
+  
 });
 
 bot.login(process.env.TOKEN);
- 
