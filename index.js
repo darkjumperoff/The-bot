4ef751be8295fd0c45148805329a28551aa7e85e
darkjumperoff <46317637+darkjumperoff@users.noreply.github.com>
2019-01-02 14:02:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,5 @@
 const Discord = require("discord.js");
 
-var PREEFIX = "D!";
-
-var bot = new Discord.Client();
-
-bot.on("ready", function() {
-    bot.user.setGame("The bot, D!help");
-    console.log("Le bot est connecté");
-});
-
-bot.once("message", async function (message){
-    if (message.author.equals(bot.user)) return; 
-
-    if(!message.content.startsWith(PREFIX)) return; 
-
-    var args = message.content.substring(PREFIX.length).split(" ");
-
-    switch(args[0].toLowerCase()) {
-
-    }
-case "invite":
-message.channel.send("",{
-   embed:{
-       color: 0xF0000
-       author:message.author.name
-       title:'',
-       fields:[{
-           name:"come test me"
-           value:"https://discord.gg/gFXxhyw"
-           inline: false
-       }]
-       footer: {
-           footer:"share this link",
-        
-       },
-   } 
-});
-
-break;
-
-//BREAK
-//DEFAULT
-
-
-
-
-
-const Discord = require("discord.js");
-
 var PREFIX = "!";
 
 var bot = new Discord.Client();
